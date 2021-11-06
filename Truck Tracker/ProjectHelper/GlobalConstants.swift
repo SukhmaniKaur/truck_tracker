97c6986fd8f1f43be38be4035db37575503d0b41
Sukhmani Kaur <sukhmanikaur.appknit@gmail.com>
2021-11-06 21:40:03 +0530

    api enum added

---
+++ Truck Tracker/ProjectHelper/GlobalConstants.swift
@@ -0,0 +1,13 @@
+//
+//  GlobalConstants.swift
+//  Truck Tracker
+//
+//  Created by MACBOOK on 06/11/21.
+//
+
+import Foundation
+
+//MARK: - MAP_API_KEY
+enum MAP_API_KEY: String {
+    case key = "AIzaSyCCw6-syRJVpK7fFsYOO8BJpfkdh5wId2A"
+}
