796bfe6d7898aaa894cf4f2e9a1f50090ca4bab5
Sukhmani Kaur <sukhmanikaur.appknit@gmail.com>
2021-11-06 22:11:41 +0530

    table view delegate methods added to list vc

--- Truck Tracker/View/Main/ListVC.swift
+++ Truck Tracker/View/Main/ListVC.swift
@@ -18,11 +18,30 @@ class ListVC: UIViewController {
     
     //MARK: - configUI
     private func configUI() {
-        
+        tableView.register(UINib(nibName: TABLE_VIEW_CELL.ListCell.rawValue, bundle: nil), forCellReuseIdentifier: TABLE_VIEW_CELL.ListCell.rawValue)
     }
     
     //MARK: - mapBtnIsPressed
     @IBAction func mapBtnIsPressed(_ sender: UIBarButtonItem) {
         self.navigationController?.popViewController(animated: true)
     }
+}
+
+//MARK: - UITableView Delegate and DataSource Methods
+extension ListVC: UITableViewDelegate, UITableViewDataSource {
+    // numberOfRowsInSection
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
+    }
+    
+    // heightForRowAt
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        80
+    }
+    
+    // cellForRowAt
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TABLE_VIEW_CELL.ListCell.rawValue, for: indexPath) as? ListCell else { return UITableViewCell() }
+        return cell
+    }    
 }
