3788400d717bc876c3f9ffbbce9f627225ddd15f
Sukhmani Kaur <sukhmanikaur.appknit@gmail.com>
2021-11-06 22:02:31 +0530

    updated const file

--- Truck Tracker/ProjectHelper/GlobalConstants.swift
+++ Truck Tracker/ProjectHelper/GlobalConstants.swift
@@ -6,8 +6,19 @@
 //
 
 import Foundation
+import UIKit
+
+//MARK: - STORYBOARD
+struct STORYBOARD {
+    static let MAIN = UIStoryboard(name: "Main", bundle: nil)
+}
 
 //MARK: - MAP_API_KEY
 enum MAP_API_KEY: String {
     case key = "AIzaSyCCw6-syRJVpK7fFsYOO8BJpfkdh5wId2A"
+}
+
+//MARK: - TABLE_VIEW_CELL
+enum TABLE_VIEW_CELL: String {
+    case ListCell
 }
