8ce5d564778252f24a4af87e8a249a96a51d91bf
Sukhmani Kaur <sukhmanikaur.appknit@gmail.com>
2021-11-06 21:48:47 +0530

    outlets added

---
+++ Truck Tracker/View/Main/ListVC.swift
@@ -0,0 +1,23 @@
+//
+//  ListVC.swift
+//  Truck Tracker
+//
+//  Created by MACBOOK on 06/11/21.
+//
+
+import UIKit
+
+class ListVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func mapBtnIsPressed(_ sender: UIBarButtonItem) {
+    }
+
+}
