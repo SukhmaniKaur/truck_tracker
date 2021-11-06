c7ade211054ce0155cf028d9c7ea8868b54fdf3a
Sukhmani Kaur <sukhmanikaur.appknit@gmail.com>
2021-11-06 22:38:40 +0530

    set up done for api implementation

---
+++ Truck Tracker/ProjectHelper/API_Helper.swift
@@ -0,0 +1,13 @@
+//
+//  API_Helper.swift
+//  Truck Tracker
+//
+//  Created by MACBOOK on 06/11/21.
+//
+
+import Foundation
+
+//MARK: - API
+struct API {
+    static let BASE_URL = "https://api.mystral.in/tt/mobile/logistics/searchTrucks?auth-company=PCH&companyId=33&deacti vated=false&key=g2qb5jvucg7j8skpu5q7ria0mu&q-expand=true&q-include=lastRunningState,last Waypoint"
+}
---
+++ Truck Tracker/ProjectHelper/API_Manager.swift
@@ -0,0 +1,173 @@
+//
+//  API_Manager.swift
+//  Truck Tracker
+//
+//  Created by MACBOOK on 06/11/21.
+//
+
+import Foundation
+import SystemConfiguration
+import Alamofire
+import SainiUtils
+
+public class APIManager {
+    
+    static let sharedInstance = APIManager()
+    
+    class func isConnectedToNetwork() -> Bool {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        return (isReachable && !needsConnection)
+    }
+    
+    func getMultipartHeader() -> [String:String]{
+        return ["Content-Type":"multipart/form-data"]
+    }
+    
+    func getJsonHeader() -> [String:String]{
+        return ["Content-Type":"application/json"]
+    }
+    
+    func getJsonHeaderWithToken() -> [String:String]{
+        if let token = UserDefaults.standard.value(forKey: USER_DEFAULT_KEYS.token.rawValue) as? String {
+            return ["Content-Type":"application/json", "Authorization": token]
+        }
+        else {
+            return ["Content-Type":"application/json", "Authorization":AppModel.shared.token]
+        }
+    }
+    
+    func getMultipartHeaderWithToken() -> [String:String]{
+        if let token = UserDefaults.standard.value(forKey: USER_DEFAULT_KEYS.token.rawValue) as? String {
+            return ["Content-Type":"multipart/form-data", "Authorization": token]
+        }
+        else {
+            return ["Content-Type":"multipart/form-data", "Authorization":AppModel.shared.token]
+        }
+    }
+    
+    func getx_www_orm_urlencoded() -> [String:String]{
+        if let token = UserDefaults.standard.value(forKey: USER_DEFAULT_KEYS.token.rawValue) as? String {
+            return ["Content-Type":"x-www-form-urlencoded", "Authorization": token]
+        }
+        else {
+            return ["Content-Type":"x-www-form-urlencoded", "Authorization":AppModel.shared.token]
+        }
+        
+    }
+    
+    func networkErrorMsg()
+    {
+        log.error("You are not connected to the internet")/
+        displayToast("You are not connected to the internet")
+    }
+    
+    //MARK:- ERROR CODES
+    func handleError(errorCode: Int) -> String {
+        switch errorCode {
+        case 101:
+            return "Missing Required Properties"
+        case 102:
+            return "Connection Error"
+        case 103:
+            return "Requested user not found"
+        case 104:
+            return "Username/Password error"
+        case 105:
+            return "Nothing Modified/ No changes Made"
+        case 106:
+            return "Invalid Access Token"
+        case 107:
+            return "This Email id is already registered."
+        case 108:
+            return "Invalid OTP type."
+        case 109:
+            return "Token not verified."
+        case 110:
+            return "Email id already verified."
+        case 111:
+            return "Verficiation code try has been expired. Request a new token."
+        case 112:
+            return "verification code has been expired. Token expires in 24 hours."
+        case 113:
+            return "Invlid URL provided for verification."
+        case 114:
+            return "Broken reference found."
+        case 115:
+            return "Profile seems to have missing region data or you are trying to post in wrong region."
+        case 400:
+            return "Malformed Authorization token error when token in invalid or has been expired."
+        case 500:
+            return "Generic error or some default error"
+            
+        default:
+            return ""
+        }
+        
+    }
+    
+    //MARK:- I_AM_COOL_GET
+    func I_AM_COOL_GET(params: [String: Any], api: String, Loader: Bool, isMultipart:Bool, _ completion: @escaping (_ dictArr: Data?) -> Void){
+        if !APIManager.isConnectedToNetwork()
+        {
+            APIManager().networkErrorMsg()
+            return
+        }
+        
+        if Loader == true{
+            DispatchQueue.main.async {
+               showLoader()
+            }
+        }
+        
+        var headerParams :[String : String] = [String: String]()
+        var Params:[String: Any] = [String: Any]()
+        if isMultipart == true{
+            headerParams = getMultipartHeaderWithToken()
+            Params["data"] = toJson(params)
+        }
+        else{
+            headerParams  = getJsonHeaderWithToken()
+            Params = params
+        }
+        log.success("WORKING_THREAD:->>>>>>> \(Thread.current.threadName)")/
+        log.info("HEADERS: \(Log.stats()) \(headerParams)")/
+        log.info("API: \(Log.stats()) \(api)")/
+        log.info("PARAMS: \(Log.stats()) \(Params)")/
+        
+        
+        Alamofire.request(api, method: .get, encoding: JSONEncoding.default, headers: headerParams).responseJSON { (response) in
+            
+            DispatchQueue.main.async {
+                removeLoader()
+            }
+            
+            switch response.result {
+            case .success:
+                log.result("\(String(describing: response.result.value))")/
+                log.ln("prettyJSON Start \n")/
+                log.result("\(String(describing: response.data?.sainiPrettyJSON))")/
+                log.ln("prettyJSON End \n")/
+              DispatchQueue.main.async {
+                    completion(response.data)
+                }
+            case .failure(let error):
+                log.error("\(Log.stats()) \(error)")/
+                
+                break
+            }
+        }
+    }
+}
