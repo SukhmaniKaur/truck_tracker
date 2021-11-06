54016ca790859316232250c270aec542d75b4a21
Sukhmani Kaur <sukhmanikaur.appknit@gmail.com>
2021-11-06 21:46:50 +0530

    modified code of map vc

--- Truck Tracker/View/MapVC.swift
+++ Truck Tracker/View/MapVC.swift
@@ -10,24 +10,19 @@ import GoogleMaps
 
 class MapVC: UIViewController {
 
+    // OUTLETS
     @IBOutlet weak var mapView: GMSMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configUI()
     }
     
+    //MARK: - configUI
+    private func configUI() {
+        
+    }
+    
+    //MARK: - listBtnIsPressed
     @IBAction func listBtnIsPressed(_ sender: UIBarButtonItem) {
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
